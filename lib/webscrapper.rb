176754b76017a82cd49dc890426bad0de5c9e7c6
mberning <mike@Mikes-MacBook-Pro.local>
2014-07-02 19:39:52 -0400

    build out basic logging and config loading

--- lib/webscrapper.rb
+++ lib/webscrapper.rb
@@ -1,3 +1,13 @@
 module Webscrapper
+  require 'logger'
+  require 'yaml'
 
+  example_config_file = File.expand_path(File.join('..', 'config', 'example.config.yml'), File.dirname(__FILE__))
+  user_config_file = File.expand_path(File.join('..', 'config', 'config.yml'), File.dirname(__FILE__))
+  valid_files = [example_config_file, user_config_file].select { |f| File.exists?(f) }
+
+  APP_CONFIG = YAML.load_file(valid_files.first)
+
+  LOG = Logger.new(STDOUT)
+  LOG.level = Logger::DEBUG
 end
--- test/test_module_exists.rb
+++ test/test_module_exists.rb
@@ -5,6 +5,20 @@ class TestWebscrapperModule < Test::Unit::TestCase
   def test_module_exists
     assert Webscrapper.is_a?(Module)
   end
+
+  def test_config_exists
+    assert Webscrapper.const_defined?(:APP_CONFIG)
+  end
+
+  def test_config_valid
+    assert Webscrapper::APP_CONFIG.keys.include?('sites')
+    assert Webscrapper::APP_CONFIG.keys.size > 0
+  end
+
+  def test_logger
+    assert Webscrapper.const_defined?(:LOG)
+    assert Webscrapper::LOG.info('test log')
+  end
 end
 
 
